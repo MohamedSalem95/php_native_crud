52ce8124e1e1677461c9cbe10261ec473a795885
mohamed salem <drakelogan52@gmail.com>
2023-09-09 13:18:51 +0300

    comment

--- delete.php
+++ delete.php
@@ -7,5 +7,5 @@ $query = "DELETE FROM products WHERE id = {$id}";
 
 mysqli_query($conn, $query);
 mysqli_close($conn);
-
+// comment
 header('Location: http://127.0.0.1/crud?msg="Product deleted successfully."');
